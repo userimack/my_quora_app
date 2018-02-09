04862dffeccef7a13958462b0051e43276828d53
Mahendra Yadav <mahendra@azrisolutions.com>
2018-02-10 00:14:23 +0530

    Update models

--- qa/admin.py
+++ qa/admin.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from qa.models import Question, QuestionRating, Answer, AnswerRating
+from qa.models import Question, Answer, Category
 
 admin.site.register(Question)
-admin.site.register(QuestionRating)
 admin.site.register(Answer)
-admin.site.register(AnswerRating)
+admin.site.register(Category)
--- qa/models.py
+++ qa/models.py
@@ -2,6 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    """
+    Model representation for category
+    """
+    category = models.CharField(max_length=200, help_text="Enter the category")
+
+    def __str__(self):
+        return self.category
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
 class Question(models.Model):
     """
     Model representation of an asked question
@@ -9,45 +22,21 @@ class Question(models.Model):
     subject = models.CharField(max_length=200, help_text="Enter the subject of the question.")
     description = models.TextField(help_text="Enter the description of your question.")
     contributer = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0, help_text="Rate the question")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, help_text="Choose category")
 
     def __str__(self):
-        """
-        String for representing the Model object (in Admin site etc.)
-        """
         return self.subject
 
 
 class Answer(models.Model):
     """
-    Model representation of answer to aksed question
+    Model representation of answer to the asked questions
     """
     question = models.ForeignKey(Question, help_text="Select question", on_delete=models.CASCADE)
     answer = models.TextField(help_text="Enter your answer")
     contributer = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0, help_text="Rate the answer")
 
     def __str__(self):
         return "{}...".format(self.answer[0:10])
-
-
-class QuestionRating(models.Model):
-    """
-    To store ratings for questions
-    """
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    rating = models.IntegerField(default=0, help_text="Rate the question")
-    contributer = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.question.id} ({self.question.subject})'
-
-
-class AnswerRating(models.Model):
-    """
-    To store ratings for questions
-    """
-    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
-    rating = models.IntegerField(default=0, help_text="Rate the answer")
-    contributer = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.answer.id} (Answer of {self.answer.question.subject})'
