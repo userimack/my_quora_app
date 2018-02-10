1ace8f89fa5ef23b28caf72fc5ddeaf8cf743cb1
Mahendra Yadav <12096062+userimack@users.noreply.github.com>
2018-02-10 09:50:57 +0530

    Spelling correction

--- qa/models.py
+++ qa/models.py
@@ -6,10 +6,10 @@ class Category(models.Model):
     """
     Model representation for category
     """
-    category = models.CharField(max_length=200, help_text="Enter the category")
+    name = models.CharField(max_length=200, help_text="Enter the category")
 
     def __str__(self):
-        return self.category
+        return self.name
 
     class Meta:
         verbose_name_plural = "Categories"
@@ -21,7 +21,7 @@ class Question(models.Model):
     """
     subject = models.CharField(max_length=200, help_text="Enter the subject of the question.")
     description = models.TextField(help_text="Enter the description of your question.")
-    contributer = models.ForeignKey(User, on_delete=models.CASCADE)
+    contributor = models.ForeignKey(User, on_delete=models.CASCADE)
     rating = models.IntegerField(default=0, help_text="Rate the question")
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, help_text="Choose category")
 
@@ -35,7 +35,7 @@ class Answer(models.Model):
     """
     question = models.ForeignKey(Question, help_text="Select question", on_delete=models.CASCADE)
     answer = models.TextField(help_text="Enter your answer")
-    contributer = models.ForeignKey(User, on_delete=models.CASCADE)
+    contributor = models.ForeignKey(User, on_delete=models.CASCADE)
     rating = models.IntegerField(default=0, help_text="Rate the answer")
 
     def __str__(self):
