9224fca3c20f253f86faa585b6cd74d713afc1b0
Mahendra Yadav <mahendra@beautifulcode.in>
2018-03-21 13:10:44 +0530

    Add test for models upvote/downvote logic

--- qa/tests/test_models.py
+++ qa/tests/test_models.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.models import User
-from django.test import Client
 import pytest
 
 from qa.models import Question
@@ -18,35 +17,29 @@ class TestDatabaseOperation():
         self.question.delete()
         self.user.delete()
 
-    def test_user_creation(self):
-        client = Client()
-        username = "test"
-        password = "test123"
-
-        User(
-            username=username, password=password)
-        #  user.save()
-        client.login(username=username, password=password)
-        response = client.get("/")
-        assert response.status_code == 200
-
     def test_question_contibutor(self):
         assert self.user == self.question.contributor
 
+    # upvotes
     def test_question_upvote_when_no_upvotes_are_present(self):
         self.question.upvote(self.user)
         assert self.user in self.question.upvoted_by_users.all()
 
-    def test_question_upvotes_count_when_a_user_upvotes_and_no_upvotes_are_present(self):
+    def test_question_upvotes_count_when_no_upvotes_are_present(self):
         self.question.upvote(self.user)
         assert self.question.total_upvotes == 1
 
-    # TODO test for question upvotes count whhen a user has already upvoted
+    def test_question_upvotes_count_when_a_user_has_already_upvoted(self):
+        self.question.upvote(self.user)
+        self.question.upvote(self.user)
+        assert self.question.total_upvotes == 1
+
     def test_question_upvotes_count_when_a_user_upvotes_and_upvotes_are_present(self):
         self.user1 = User(username="test1", password="test123")
         self.user1.save()
         self.question.upvote(self.user1)
         self.question.upvote(self.user)
+        self.user1.delete()
         assert self.question.total_upvotes == 2
 
     def test_question_downvotes_count_when_a_user_upvotes_and_no_upvotes_are_present(self):
@@ -62,10 +55,30 @@ class TestDatabaseOperation():
         self.question.upvote(self.user)
         assert self.question.total_downvotes == old_total_downvotes_count
 
+    def test_question_upvotes_count_when_a_user_downvotes_and_upvotes(self):
+        self.question.downvote(self.user)
+        self.question.upvote(self.user)
+        assert self.question.total_upvotes == 1
+
     def test_question_downvotes_count_when_a_user_downvotes_and_upvotes(self):
         self.question.downvote(self.user)
         self.question.upvote(self.user)
+        assert self.question.total_downvotes == 0
+
+    def test_question_upvotes_count_when_a_user_upvotes_and_downvotes(self):
+        self.question.upvote(self.user)
+        self.question.downvote(self.user)
+        assert self.question.total_upvotes == 0
+
+    def test_question_downvotes_count_when_a_user_upvotes_and_downvotes(self):
+        self.question.upvote(self.user)
+        self.question.downvote(self.user)
         assert self.question.total_downvotes == 1
+
+    def test_question_when_user_upvotes_twice(self):
+        self.question.upvote(self.user)
+        self.question.upvote(self.user)
+        assert self.question.total_upvotes == 1
 
     # downvotes
     def test_question_downvote_when_no_downvotes_are_present(self):
@@ -96,6 +109,11 @@ class TestDatabaseOperation():
         self.question.downvote(self.user)
         assert self.question.total_upvotes == old_total_upvotes_count
 
+    def test_question_when_user_downvotes_twice(self):
+        self.question.downvote(self.user)
+        self.question.downvote(self.user)
+        assert self.question.total_downvotes == 1
+
     # testing has_upvoted / has_downvoted
     def test_question_has_upvoted_when_user_has_not_upvoted(self):
         assert not self.question.has_upvoted(self.user)
@@ -110,4 +128,3 @@ class TestDatabaseOperation():
     def test_question_has_downvoted_when_user_has_already_downvoted(self):
         self.question.downvote(self.user)
         assert self.question.has_downvoted(self.user)
-
